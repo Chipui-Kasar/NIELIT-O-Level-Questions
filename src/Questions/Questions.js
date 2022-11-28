982c581f152d84c4564501ed03cdc6ff3398dadf
Chipui <chipuikasar@gmail.com>
2022-11-28 11:28:32 +0530

    added boxes

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -1,11 +1,12 @@
 // import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Questions.css";
 import data from "./data";
 import { Link } from "react-router-dom";
 const Questions = () => {
+  const [datas, setData] = useState([]);
   useEffect(() => {
-    console.log(data);
+    setData(data);
     // axios
     //   .post(
     //     `https://app-send-data-splist.azurewebsites.net/api/send_data_to_splist?params=[{"fieldNameValue": "Name:Chipui"},{"fieldNameValue": "Email:chipui@sharepointdesigns.com"},{"fieldNameValue": "LeaderGUID:57384"},{"fieldNameValue": "LeadersurveyID:35325"},{"fieldNameValue": "ObserversurveyID:58982"}]&body={"listName": "Leaderssurveyemail","listUrl":"https://randcome.sharepoint.com/sites/KnowledgeSharing/_api/web/lists/GetByTitle('Leaderssurveyemail')/items"}`
@@ -20,20 +21,55 @@ const Questions = () => {
   }, []);
 
   return (
-    <div
-      style={{
-        height: "90vh",
-        display: "flex",
-        placeContent: "center",
-        placeItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1> Waiting for the design </h1>
-
+    <div className="questions">
+      {/* <h1> Waiting for the design </h1> */}
+      {/* <marquee> Hover to preview the file & click to download</marquee> */}
       <Link to="/" className="goBack">
         Go Back
       </Link>
+      <div className="container">
+        {datas.length > 0 &&
+          datas.map((res, key) => {
+            return (
+              <div className="questionBox" key={key}>
+                <b>{res.subject}</b>
+                <ol>
+                  {res.details.map((sub, key) => {
+                    let splitUrl = "https://drive.google.com/file/d/";
+                    let id = sub.url.split(splitUrl)[1].split("/")[0];
+                    return (
+                      <li key={key} className="items">
+                        <a
+                          href={
+                            "https://drive.google.com/u/0/uc?id=" +
+                            id +
+                            "&export=download"
+                          }
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          {sub.title}
+                        </a>
+
+                        <div className="pdfContent">
+                          <iframe
+                            className="fileViewer"
+                            src={sub.url.split("/view")[0] + "/preview"}
+                            embedded="true"
+                            width="300px"
+                            height="300px"
+                            frameBorder="0"
+                            title={sub.title}
+                          ></iframe>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ol>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
--- src/Questions/data.js
+++ src/Questions/data.js
@@ -1,58 +1,69 @@
-const Questions = {
-  IOT: [
-    {
-      url: "https://drive.google.com/file/d/1IosJJXlvRB718p-Tx5XZc2pPEZK3Jf09/view?usp=sharing",
-      title: "IOT January 2021 (Solved)",
-      subjectCode: "",
-    },
-    {
-      url: "https://drive.google.com/file/d/1eHyCNzYpAV_BIW1qmOgZuXuJE4nVNxzK/view?usp=sharing",
-      title: "IOT Septermber 2021 (Solved)",
-      subjectCode: "",
-    },
-  ],
-
-  Python: [
-    {
-      url: "https://drive.google.com/file/d/1lXOXVAFe2ehiDEv4bbZPnxUVLq81ILJG/view?usp=share_link",
-      title: "Python January 2021 (Solved)",
-      subjectCode: "",
-    },
-    {
-      url: "https://drive.google.com/file/d/1ygCnhRli6WaEJ4F4TVP3vPZaa52u7x1C/view?usp=share_link",
-      title: "Python September 2021 (Solved)",
-      subjectCode: "",
-    },
-  ],
-  ITNB: [
-    {
-      url: "https://drive.google.com/file/d/1gkx57kkZUL1Tl3LNmA3V-WeFeEc2RdhE/view?usp=share_link",
-      title: "IT Tools & Network 2022",
-      subjectCode: "",
-    },
-    {
-      url: "https://drive.google.com/file/d/19JudSPEeJ6GKSlV81F1EXCx33gWY1oOc/view?usp=share_link",
-      title: "IT Tools & Network January 2021",
-      subjectCode: "",
-    },
-    {
-      url: "https://drive.google.com/file/d/1lVfthcmoljYevU8_D2EvKNnIVJWLYaQy/view?usp=share_link",
-      title: "IT Tools & Network September 2021",
-      subjectCode: "",
-    },
-  ],
-  WDP: [
-    {
-      url: "https://drive.google.com/file/d/1-Nm-UBFsHFI2Dz2NcbhJX0Zo3JyaSGXu/view?usp=share_link",
-      title: "WDP January 2021 (Solved)",
-      subjectCode: "",
-    },
-    {
-      url: "https://drive.google.com/file/d/1w0Uc-95H241VY-BUUHePx4EC3lECyLdI/view?usp=share_link",
-      title: "WDP September 2021 (Solved)",
-      subjectCode: "",
-    },
-  ],
-};
+const Questions = [
+  {
+    subject: "IOT",
+    details: [
+      {
+        url: "https://drive.google.com/file/d/1IosJJXlvRB718p-Tx5XZc2pPEZK3Jf09/view?usp=sharing",
+        title: "IOT January 2021 (Solved)",
+        subjectCode: "",
+      },
+      {
+        url: "https://drive.google.com/file/d/1eHyCNzYpAV_BIW1qmOgZuXuJE4nVNxzK/view?usp=sharing",
+        title: "IOT Septermber 2021 (Solved)",
+        subjectCode: "",
+      },
+    ],
+  },
+  {
+    subject: "Python",
+    details: [
+      {
+        url: "https://drive.google.com/file/d/1lXOXVAFe2ehiDEv4bbZPnxUVLq81ILJG/view?usp=share_link",
+        title: "Python January 2021 (Solved)",
+        subjectCode: "",
+      },
+      {
+        url: "https://drive.google.com/file/d/1ygCnhRli6WaEJ4F4TVP3vPZaa52u7x1C/view?usp=share_link",
+        title: "Python September 2021 (Solved)",
+        subjectCode: "",
+      },
+    ],
+  },
+  {
+    subject: "ITNB",
+    details: [
+      {
+        url: "https://drive.google.com/file/d/1gkx57kkZUL1Tl3LNmA3V-WeFeEc2RdhE/view?usp=share_link",
+        title: "IT Tools & Network 2022",
+        subjectCode: "",
+      },
+      {
+        url: "https://drive.google.com/file/d/19JudSPEeJ6GKSlV81F1EXCx33gWY1oOc/view?usp=share_link",
+        title: "IT Tools & Network January 2021",
+        subjectCode: "",
+      },
+      {
+        url: "https://drive.google.com/file/d/1lVfthcmoljYevU8_D2EvKNnIVJWLYaQy/view?usp=share_link",
+        title: "IT Tools & Network September 2021",
+        subjectCode: "",
+      },
+    ],
+  },
+  {
+    subject: "WDP",
+    details: [
+      {
+        url: "https://drive.google.com/file/d/1-Nm-UBFsHFI2Dz2NcbhJX0Zo3JyaSGXu/view?usp=share_link",
+        title: "WDP January 2021 (Solved)",
+        subjectCode: "",
+      },
+      {
+        url: "https://drive.google.com/file/d/1w0Uc-95H241VY-BUUHePx4EC3lECyLdI/view?usp=share_link",
+        title: "WDP September 2021 (Solved)",
+        subjectCode: "",
+      },
+    ],
+  },
+];
 
 export default Questions;
